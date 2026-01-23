af12e670ecd1e74e11558e77c0aa4d547df6da42
tenhag321-bit <tenhag321@protonmail.com>
2026-01-23 10:00:14 +0545

    Node.js Understanding

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
- require('dotenv').config()
- const mongoose = require('mongoose')
+const mongoose = require('mongoose')
+
+async function connectToDatabase(){
+   await mongoose.connect(process.env.MONGODB_URL)
+   console.log("Database connected successfully")
+} 
+
+module.exports = connectToDatabase
 
 
 
- 
-  async function connectToDatabase(){
-    mongoose.connect(process.env.MONGODB_URL)
-    console.log('Success')
- }
- module.exports = connectToDatabase;
--- model/blogModel.js
+++ model/blogModel.js
@@ -1,23 +1,21 @@
 const mongoose = require('mongoose')
- const Schema = mongoose.Schema
+const Schema = mongoose.Schema
 
- const blogSchema = new Schema(
-    {
-        title : {
-            type: String
-        },
-        subtitle:{
-            type: String
-        },
-        description:{
-            type: String
-        },
-        image : {
-            type: String
-        },
-       
+const blogSchema = new Schema({
+    title : {
+        type : String, 
+        unique : true
+    }, 
+    subtitle : {
+        type : String
+    }, 
+    description : {
+        type : String
+    }, 
+    image : {
+        type : String
     }
- )
+})
 
-  const Blog = mongoose.model('Blog' , blogSchema) 
-  module.exports = Blog
+const Blog = mongoose.model('Blog',blogSchema)
+module.exports = Blog
