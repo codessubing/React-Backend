d1362e5be228773e1a42fb2557d52c94ab007c11
tenhag321-bit <tenhag321@protonmail.com>
2026-02-16 18:12:04 +0545

    hp

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const fs = require('fs')
 const cors  = require('cors')
 
 
-app.use(cors(
-    {
-        origin : ['http://localhost:5173','https://cardde-v.vercel.app/'] 
-    }
-))
-
+app.use(cors({
+  origin: 'https://cardde-v.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 connectToDatabase()
 
